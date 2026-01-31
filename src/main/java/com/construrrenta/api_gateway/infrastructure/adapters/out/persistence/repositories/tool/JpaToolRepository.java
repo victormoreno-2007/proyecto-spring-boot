01bfaaf0748b0381daccbe6c4de54ab8a8eb1f96
Marcela Albarracin <lmalbarracin57@gmail.com>
2026-01-31 14:07:14 -0500

    fix: :bug: solucion conflicto

--- src/main/java/com/construrrenta/api_gateway/infrastructure/adapters/out/persistence/repositories/tool/JpaToolRepository.java
+++ src/main/java/com/construrrenta/api_gateway/infrastructure/adapters/out/persistence/repositories/tool/JpaToolRepository.java
@@ -1,24 +1,14 @@
 package com.construrrenta.api_gateway.infrastructure.adapters.out.persistence.repositories.tool;
 
-<<<<<<< Updated upstream
-=======
 import java.util.List;
->>>>>>> Stashed changes
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-<<<<<<< Updated upstream
-import com.construrrenta.api_gateway.infrastructure.adapters.out.entities.ToolEntity;
-
-public interface JpaToolRepository extends JpaRepository<ToolEntity, UUID>{
-    
-=======
 import com.construrrenta.api_gateway.domain.model.tool.ToolStatus;
 import com.construrrenta.api_gateway.infrastructure.adapters.out.entities.ToolEntity;
 
 public interface JpaToolRepository extends JpaRepository<ToolEntity, UUID> {
     // Spring crea el SQL automático: SELECT * FROM tools WHERE status = ?
     List<ToolEntity> findByStatus(ToolStatus status);
->>>>>>> Stashed changes
 }
