e979197d0b0b59a3a61b58a2455146620bde383a
Marcela Albarracin <lmalbarracin57@gmail.com>
2026-01-31 18:16:59 -0500

    feat: :sparkles: Se implementa CRUD de proveedores

--- src/main/java/com/construrrenta/api_gateway/infrastructure/adapters/out/persistence/repositories/tool/JpaToolRepository.java
+++ src/main/java/com/construrrenta/api_gateway/infrastructure/adapters/out/persistence/repositories/tool/JpaToolRepository.java
@@ -11,4 +11,5 @@ import com.construrrenta.api_gateway.infrastructure.adapters.out.entities.ToolEn
 public interface JpaToolRepository extends JpaRepository<ToolEntity, UUID> {
     // Spring crea el SQL automático: SELECT * FROM tools WHERE status = ?
     List<ToolEntity> findByStatus(ToolStatus status);
+    List<ToolEntity> findByProviderId(UUID providerId);
 }
