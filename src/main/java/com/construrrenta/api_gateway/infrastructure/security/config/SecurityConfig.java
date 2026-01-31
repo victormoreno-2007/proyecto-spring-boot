280c636c86103c522f6ee199f2a39651484cb00c
victor moreno <victormorenosamur2007@gmail.com>
2026-01-31 18:08:43 -0500

    feat: :sparkles: crud de users para el admin

--- src/main/java/com/construrrenta/api_gateway/infrastructure/security/config/SecurityConfig.java
+++ src/main/java/com/construrrenta/api_gateway/infrastructure/security/config/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> {
                 auth.requestMatchers("/api/v1/auth/**").permitAll();
+                auth.requestMatchers("/api/v1/users/**").hasRole("ADMIN");
                 auth.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll();
                 auth.anyRequest().authenticated();
             })
