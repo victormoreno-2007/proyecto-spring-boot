8df00de65a8a14dc1dfb036aaa83040b07a0b42b
Joan Quintero <sebasq39@gmail.com>
2026-02-05 15:33:35 -0500

    feat: :sparkles: modulo de pagos inicial

--- src/main/java/com/construrrenta/api_gateway/infrastructure/security/config/SecurityConfig.java
+++ src/main/java/com/construrrenta/api_gateway/infrastructure/security/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.http.HttpMethod;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
